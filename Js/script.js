c7a95edaa950a543ccb009fd2476723f8baa50d2
yetemegn-telaye <75949338+yetemegn-telaye@users.noreply.github.com>
2022-09-23 20:43:55 +0300

    Add more button functionality and Finalize project

--- Js/script.js
+++ Js/script.js
@@ -10,8 +10,8 @@ const speakersData = [
   {
     speakerImg: "/images/profile-img3.jpg",
     speakerName: "Bob Scheldon",
-    speakerTitle: "Director of Art Centre Nabi and board member of CC Korea",
-    speakerDetail: "As the main venue for new media art production in Korea, Nabi promotes cross-disciplinary collaboration and understanding among science technology, humanitites, and the arts."
+    speakerTitle: "Berkman Professor of Entrepreneurial Studies at Harvard",
+    speakerDetail: "As the main venue for new media art production in UK."
   },
   {
     speakerImg: "/images/profile-img3.jpg",
@@ -45,28 +45,58 @@ menuCloseBtn.addEventListener('click', () => {
   toggleMenuBtn.ariaExpanded = false;
 });
 
-function displaySpeakerCard (speakersData) {
-  speakersData.forEach(speaker => {
-    speakerColumns +=`
-    <div class="col-12 col-md-6">
-    <div class="d-flex justify-content-center align-items-start">
-        <div class="speaker-img">
-            <img src=${speaker.speakerImg} class="img-fluid"/>
-        </div>
-       
-        <div class="text-start ms-3 speaker-detail">
-            <h3>${speaker.speakerName}</h3>
-            <p class="common-h3 fst-italic">${speaker.speakerTitle}</p>
-            <hr class="speaker-hr">
-            <p class="fs-6">
-               <small>${speaker.speakerDetail}</small>
-            </p>
-        </div>
-    </div>
-    </div>
-    `;
-  });
+const mediaQuery = window.matchMedia('(min-width: 768px)');
+function displaySpeakerCard (data) {
+
+    data.forEach(speaker => {
+      speakerColumns +=`
+      <div class="col-12 col-md-6">
+      <div class="d-flex justify-content-center align-items-start">
+          <div class="speaker-img">
+              <img src=${speaker.speakerImg} class="speaker-img"/>
+          </div>
+         
+          <div class="text-start ms-3 speaker-detail">
+              <h5 class="fw-semibold">${speaker.speakerName}</h5>
+              <p class="text-color-orange fst-italic lh-sm fs-6">${speaker.speakerTitle}</p>
+              <hr class="speaker-hr">
+              <p class="fs-6">
+                 <small>${speaker.speakerDetail}</small>
+              </p>
+          </div>
+      </div>
+      </div>
+      `;
+    });
+  
+  
   rowContainer.innerHTML=speakerColumns;
 }
 
-displaySpeakerCard(speakersData);
+
+let slicedSpeakersData = speakersData.slice(0,2);
+
+function handleTabletChange(e) {
+  
+  if (e.matches) {
+    speakerColumns = "";
+    displaySpeakerCard(speakersData);
+    console.log("Desktop screen");
+  }
+  else{
+    speakerColumns = "";
+    displaySpeakerCard(slicedSpeakersData);
+  }
+}
+
+mediaQuery.addEventListener('change',handleTabletChange)
+
+handleTabletChange(mediaQuery);
+
+let moreBtn = document.querySelector("#more-btn");
+let afterSlicedSpeakers = speakersData.slice(2,speakersData.length);
+
+moreBtn.addEventListener('click', function(){
+  displaySpeakerCard(afterSlicedSpeakers);
+  moreBtn.style.display= "none";
+});
