03457fa4de6b7131bcafc475ba0e1657a398ea69
yetemegn-telaye <75949338+yetemegn-telaye@users.noreply.github.com>
2022-09-23 12:46:27 +0300

    Fix linter errors

--- Js/script.js
+++ Js/script.js
@@ -1,9 +1,6 @@
-const menuCloseBtn = document.querySelector("#menu-close");
+const menuCloseBtn = document.querySelector('#menu-close');
 
-
-menuCloseBtn.addEventListener('click', function(){
-    let toggleMenuBtn = document.querySelector("#toggleMenuBtn");
-    toggleMenuBtn.ariaExpanded=false;
-    console.log(toggleMenuBtn.ariaExpanded);
+menuCloseBtn.addEventListener('click', () => {
+  const toggleMenuBtn = document.querySelector('#toggleMenuBtn');
+  toggleMenuBtn.ariaExpanded = false;
 });
-
